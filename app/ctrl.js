8e573f08a30112c3f086c9da97a171276e2092c2
Dheekshita Kumar <dhkumar@mit.edu>
2017-01-04 18:16:31 -0500

    added images to html. will style later

--- app/ctrl.js
+++ app/ctrl.js
@@ -1,11 +1,41 @@
 var app = angular.module('MainApp', []);
 
-app.controller('Ctrl', function($scope, $http) {
+/*
+Search from
+https://codepen.io/shahansha/pen/pvWJyv?editors=1010
+*/
 
+app.filter('searchFor', function(){
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+
+		searchString = searchString.toLowerCase();
+
+		angular.forEach(arr, function(company){
+
+			if(company.gsx$name.$t.toLowerCase().indexOf(searchString) !== -1 || company.gsx$snippet.$t.toLowerCase().indexOf(searchString) !== -1){
+				result.push(company);
+			}
+
+		});
+
+		return result;
+	};
+
+});
+
+app.controller('Ctrl', function($scope, $http) {
+	
 	orderProp = 'boothnumber';
 	
 	$http.get("https://spreadsheets.google.com/feeds/list/1h9qPXGp2fWQNPSnYGvruMH85sY1YCqpoE42nBRksFAo/1/public/values?alt=json")
-        .success(function(response) {
+		.success(function(response) {
 		$scope.companies = response.feed.entry;
 	});
 });
