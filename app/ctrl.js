e155628fc6163524c2fade388558cc65be2c7d48
Dheekshita Kumar <dhkumar@mit.edu>
2017-01-04 14:42:27 -0500

    intial commit

--- app/ctrl.js
+++ app/ctrl.js
@@ -1,25 +1,11 @@
 var app = angular.module('MainApp', []);
 
 app.controller('Ctrl', function($scope, $http) {
-	/*$scope.companies = [
-		{
-          name: 'Google',
-          snippet: 'Machine learning',
-          boothnumber: 1
-        }, {
-          name: 'Oracle',
-          snippet: 'With more than 420,000 customers and deployments in more than 145 countries, Oracle offers a comprehensive and fully integrated stack of cloud applications, platform services, and engineered systems. ',
-          boothnumber: 2
-        }, {
-          name: 'Facebook',
-          snippet: 'Make the world more open and connected. Machine learning.',
-          boothnumber: 3
-        }
-	];*/
 
 	orderProp = 'boothnumber';
 	
-	$http.get('companies/companies2.json').success(function(data) {
-		$scope.companies = data;
+	$http.get("https://spreadsheets.google.com/feeds/list/1h9qPXGp2fWQNPSnYGvruMH85sY1YCqpoE42nBRksFAo/1/public/values?alt=json")
+        .success(function(response) {
+		$scope.companies = response.feed.entry;
 	});
 });
