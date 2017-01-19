5ccfec955fb0547f0a2b06e551c889a77f044320
Dheekshita Kumar <dhkumar@mit.edu>
2017-01-18 19:38:42 -0500

    Favorites debug

--- app/ctrl.js
+++ app/ctrl.js
@@ -6,36 +6,42 @@ https://codepen.io/shahansha/pen/pvWJyv?editors=1010
 */
 
 app.filter('searchFor', function(){
-
 	return function(arr, searchString){
-
 		if(!searchString){
 			return arr;
 		}
-
 		var result = [];
-
 		searchString = searchString.toLowerCase();
-
 		angular.forEach(arr, function(company){
-
-			if(company.gsx$name.$t.toLowerCase().indexOf(searchString) !== -1 || company.gsx$snippet.$t.toLowerCase().indexOf(searchString) !== -1){
+			if(company.gsx$name.$t.toLowerCase().indexOf(searchString) !== -1 || company.gsx$majors.$t.toLowerCase().indexOf(searchString) !== -1 || company.gsx$positions.$t.toLowerCase().indexOf(searchString) !== -1 || company.gsx$snippet.$t.toLowerCase().indexOf(searchString) !== -1){
 				result.push(company);
 			}
-
 		});
-
 		return result;
 	};
-
 });
 
 app.controller('Ctrl', function($scope, $http) {
-	
-	orderProp = 'boothnumber';
-	
 	$http.get("https://spreadsheets.google.com/feeds/list/1h9qPXGp2fWQNPSnYGvruMH85sY1YCqpoE42nBRksFAo/1/public/values?alt=json")
 		.success(function(response) {
 		$scope.companies = response.feed.entry;
 	});
+	var $scope.favoriteList = [];
+	$scope.favoriteThis = function(company){
+		//if (favoriteList.indexOf(company) < 0)  {
+			//favoriteList.push(company);
+			$scope.favoriteList.push(company);
+			console.log(favoriteList);
+	//	};
+	};
 });
+
+/*app.controller('favCtrl', ['$scope', function($scope){
+	$scope.favoriteList = [];
+	$scope.favoriteThis = function(company){
+		//if(favoriteList.indexOf(company) !>= 0){
+			$scope.favoriteList.push(company);
+		//};
+	};
+}]);*/
+
